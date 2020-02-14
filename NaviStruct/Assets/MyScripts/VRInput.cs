60fd1f98ee62fb44c015cddd01ccab22d0367702
Bryan Lopes <brylopes89@gmail.com>
2020-02-13 23:16:26 -0500

    Physics pointer and input module added

---
+++ NaviStruct/Assets/MyScripts/VRInput.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Valve.VR;
+
+public class VRInput : BaseInput
+{
+    public Camera eventCamera = null;
+    public SteamVR_Input_Sources targetSource;
+    public SteamVR_Action_Boolean clickAction;
+
+    protected override void Awake()
+    {
+        GetComponent<BaseInputModule>().inputOverride = this;
+    }
+
+    public override bool GetMouseButton(int button)
+    {
+        return clickAction.GetState(targetSource);        
+    }
+
+    public override bool GetMouseButtonDown(int button)
+    {
+        return clickAction.GetStateDown(targetSource);
+    }
+
+    public override bool GetMouseButtonUp(int button)
+    {
+        return clickAction.GetStateUp(targetSource);
+    }
+
+    public override Vector2 mousePosition
+    {
+        get
+        {
+            return new Vector2(eventCamera.pixelWidth / 2, eventCamera.pixelHeight /2);
+        }
+    }
+}
---
+++ NaviStruct/Assets/MyScripts/VRInputModule.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Valve.VR;
+public class VRInputModule : BaseInputModule
+{
+    public Camera cam;
+    public SteamVR_Input_Sources targetSource;
+    public SteamVR_Action_Boolean clickAction;
+
+    private GameObject currentObject = null;
+    private PointerEventData data = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        data = new PointerEventData(eventSystem);
+    }
+
+    public override void Process()
+    {
+        //Reset data, set camera
+        data.Reset();
+        data.position = new Vector2(cam.pixelWidth / 2, cam.pixelHeight / 2);
+
+        //Raycast
+        eventSystem.RaycastAll(data, m_RaycastResultCache);
+        data.pointerCurrentRaycast = FindFirstRaycast(m_RaycastResultCache);
+        currentObject = data.pointerCurrentRaycast.gameObject;
+
+        //Clear Raycast
+        m_RaycastResultCache.Clear();
+
+        //Handle hover states
+        HandlePointerExitAndEnter(data, currentObject);
+
+        //Press input
+        if (clickAction.GetStateDown(targetSource))
+            ProcessPress(data);
+
+        //Release input
+        if (clickAction.GetStateUp(targetSource))
+            ProcessPress(data);
+    }
+
+    public PointerEventData GetData()
+    {
+        return data;
+    }   
+
+    private void ProcessPress(PointerEventData data)
+    {
+        //set raycast
+        data.pointerPressRaycast = data.pointerCurrentRaycast;
+
+        //Check for object hit, get the down handler, call
+        GameObject newPointerPress = ExecuteEvents.ExecuteHierarchy(currentObject, data, ExecuteEvents.pointerDownHandler);
+
+        //If no down handler, try and click handler
+        if (newPointerPress == null)
+            newPointerPress = ExecuteEvents.GetEventHandler<IPointerClickHandler>(currentObject);
+
+        //set data
+        data.pressPosition = data.position;
+        data.pointerPress = newPointerPress;
+        data.rawPointerPress = currentObject;
+    }
+
+    private void ProcessRelease(PointerEventData data)
+    {
+        //Execute pointer up
+        ExecuteEvents.Execute(data.pointerPress, data, ExecuteEvents.pointerUpHandler);
+
+        //Check for click handler
+        GameObject pointerUpHandler = ExecuteEvents.GetEventHandler<IPointerClickHandler>(currentObject);
+
+        //Check if actual
+        if(data.pointerPress == pointerUpHandler)        
+            ExecuteEvents.Execute(data.pointerPress, data, ExecuteEvents.pointerClickHandler);
+
+        //Clear selected gameobject
+        eventSystem.SetSelectedGameObject(null);
+
+        //reset data
+        data.pressPosition = Vector2.zero;
+        data.pointerPress = null;
+        data.rawPointerPress = null;
+    }
+}
+
