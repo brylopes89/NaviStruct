112aeb1fdddea33c3dbb373961e652e366d8a057
Bryan Lopes <brylopes89@gmail.com>
2020-04-10 15:50:25 -0400

    custom ARPlacementSingle script and event created

---
+++ NaviStruct/Assets/MyScripts/ARCanvasInteractionLog.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ARCanvasInteractionLog : MonoBehaviour
+{
+    //[SerializeField]
+    //private TextMeshProUGUI details;
+    
+    public void Toggle()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+}
