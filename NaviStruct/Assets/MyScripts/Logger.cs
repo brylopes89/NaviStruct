58cea70ca84bcf9abdfff9d1eb27089c63ffc5ed
Bryan Lopes <brylopes89@gmail.com>
2020-04-10 15:50:25 -0400

    custom ARPlacementSingle script and event created

---
+++ NaviStruct/Assets/MyScripts/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using TMPro;
+
+public class Logger : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshPro debugAreaText;
+
+    [SerializeField]
+    private bool enableDebug = false;
+
+    [SerializeField]
+    private int maxLines = 15;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        debugAreaText.enabled = enableDebug;
+        enabled = enableDebug;
+    }
+
+    // Update is called once per frame
+    public void LogInfo(string message)
+    {
+        
+    }
+}
---
+++ NaviStruct/Assets/MyScripts/XRExtensions/ARPlacementInteractableSingle.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.AR;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARPlacementInteractableSingle : ARBaseGestureInteractable
+{
+    [SerializeField]
+    private GameObject placementPrefab;
+
+    [SerializeField]
+    private ARObjectPlacementEvent onObjectPlaced;
+
+    private GameObject placementObject;
+
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    private static GameObject trackablesObject;
+
+    protected override bool CanStartManipulationForGesture(TapGesture gesture)
+    {
+        if (gesture.TargetObject == null)
+            return true;
+
+        return false;
+    }
+
+    protected override void OnEndManipulation(TapGesture gesture)
+    {
+        if (gesture.WasCancelled)
+            return;
+
+        if (gesture.TargetObject != null)
+            return;
+
+        if(GestureTransformationUtility.Raycast(gesture.StartPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+        {
+            var hit = hits[0];
+
+            if(Vector3.Dot(Camera.main.transform.position - hit.pose.position, hit.pose.rotation * Vector3.up) < 0)
+            {
+                return;
+            }
+
+            if(placementObject == null)
+            {
+                placementObject = Instantiate(placementPrefab, hit.pose.position, hit.pose.rotation);
+
+                var anchorObject = new GameObject("PlacementAnchor");
+                anchorObject.transform.position = hit.pose.position;
+                anchorObject.transform.rotation = hit.pose.rotation;
+
+                if(trackablesObject == null)                
+                    trackablesObject = GameObject.Find("Trackables");                
+
+                if (trackablesObject != null)
+                    anchorObject.transform.parent = trackablesObject.transform;
+
+                onObjectPlaced?.Invoke(this, placementObject);
+            }
+        }
+    }
+}
