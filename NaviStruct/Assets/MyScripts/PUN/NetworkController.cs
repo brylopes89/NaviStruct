a14d129561dc995507cb45f20c671af294620651
Bryan Lopes <brylopes89@gmail.com>
2020-04-14 20:44:43 -0400

    Photon multiplayer capabilites added. Quick game button added, along with player lobby and loading screen.

---
+++ NaviStruct/Assets/MyScripts/PUN/NetworkController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(); //Connects to Photon master servers
+        //other ways to make a connection can be found here: https://doc-api.photonengine.com/en/pun/v2/class_photon_1_1_pun_1_1_photon_network.html
+    }
+
+    // Update is called once per frame
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("We are now connected to the " + PhotonNetwork.CloudRegion + "server!");
+    }
+}
---
+++ NaviStruct/Assets/MyScripts/PUN/QuickStartRoomController.cs
@@ -0,0 +1,59 @@
+﻿using Photon.Pun;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuickStartRoomController : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private int multiplayerSceneIndex; //Number for the build index to the multiplay scene
+
+    [SerializeField]
+    private Button loader;
+
+    [SerializeField]
+    private TextMeshProUGUI roomText;
+
+    [SerializeField]
+    private QuickStartLobbyController roomNumber;
+
+    public override void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public override void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    public override void OnJoinedRoom() //Callback function for when we successfully create or join a room
+    {
+        Debug.Log("Joined Room");
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Starting Game");            
+            StartCoroutine(LoadAsynchronously(multiplayerSceneIndex));            
+        }
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        PhotonNetwork.LoadLevel(sceneIndex);
+        AsyncOperation operation = PhotonNetwork._AsyncLevelLoadingOperation;
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            loader.image.fillAmount = progress;
+            roomText.text = "Room #: " + roomNumber.randomRoomNumber;
+            yield return null;
+        }
+    }
+}
