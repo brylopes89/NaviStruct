f630aaecb6652f7f7bb830c0b08fab05a019abca
Bryan Lopes <brylopes89@gmail.com>
2020-04-18 13:11:36 -0400

    Addressing player syncing issue

---
+++ NaviStruct/Assets/MyScripts/VR/HMDHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public enum ActiveVRFamily { Oculus, Vive, Windows };
+public class HMDHandler : MonoBehaviour
+{
+    public string detectedHMD = "";
+    public ActiveVRFamily activeVRHMD;
+    public Transform leftController;   
+    public Transform rightController;
+    public Transform rightAimingNub;
+    public Transform leftAimingNub;
+
+    public Vector3 ViveOffset;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        detectedHMD = XRSettings.loadedDeviceName;
+        if (detectedHMD.ToLower().Contains("vive"))
+        {
+            // Must be a Vive headset.
+            activeVRHMD = ActiveVRFamily.Vive;
+            leftAimingNub.localEulerAngles = ViveOffset;
+            rightAimingNub.localEulerAngles = ViveOffset;
+            
+        }
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
