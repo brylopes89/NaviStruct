353dd747d75ab8af61a690b6aeeb0245fcb0a537
Bryan Lopes <brylopes89@gmail.com>
2020-08-11 19:27:56 -0400

    tweaking selecthandler script

--- NaviStruct/Assets/MyScripts/XRExtensions/XRCustomRaycast.cs
+++ NaviStruct/Assets/MyScripts/XRExtensions/XRCustomRaycast.cs
@@ -1,89 +1,112 @@
 ﻿using Photon.Pun.Demo.SlotRacer.Utils;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class XRCustomRaycast : MonoBehaviour
 {
-    public float smoothPosition = 2f;    
-
     private TeleportationArea t_Area = null;
+
     private PlayerStateManager state_Manager = null;
     private XRGrabInteractable interactable = null;   
     private XRInteractorLineVisual line_Visual = null;
     private LineRenderer line_Renderer = null;
 
+
     private Vector3[] positions;
-    private Vector3 original_Pos;
-    private Quaternion original_Rot;
-    private Rigidbody object_RB;
+    private List<XRGrabInteractable> interactables = new List<XRGrabInteractable>();
+    private List<TeleportationArea> t_Areas = new List<TeleportationArea>();
 
     private int interact_Mask = 1 << 9;
-    private void Start()
-    {
-        t_Area = FindObjectOfType<TeleportationArea>();
-        state_Manager = MasterManager.ClassReference.Avatar.GetComponent<PlayerStateManager>();        
 
-        line_Visual = GetComponent<XRInteractorLineVisual>();
-        line_Renderer = GetComponent<LineRenderer>();
+    private void Start()
+    {        
+        state_Manager = MasterManager.ClassReference.Avatar.GetComponent<PlayerStateManager>();  
+        line_Visual = this.GetComponent<XRInteractorLineVisual>();
+        line_Renderer = this.GetComponent<LineRenderer>();
         positions = new Vector3[2]; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastForInteractable();                 
+        RaycastForInteractable();
+        SetImmersiveInteractables();
     }
 
     private void RaycastForInteractable()
     {
         RaycastHit hit;
-        Ray ray = new Ray(transform.position, transform.forward);        
+        Ray ray = new Ray(transform.position, transform.forward);
 
         if (Physics.Raycast(ray, out hit, line_Visual.lineLength, interact_Mask))
         {
+            if (hit.collider.GetComponent<TeleportationArea>())
+            {
+                t_Area = hit.collider.GetComponent<TeleportationArea>();
+                t_Areas.Add(t_Area);
+            }
+
             if (state_Manager.currentState == PlayerStates.Diorama)
             {
+                //if (t_Area)
+                //TODO: Dynamically disable teleport using raycast
+
+                if (hit.collider.CompareTag("Floor"))
+                    return;
+
                 if (hit.collider.gameObject.GetComponent<XRGrabInteractable>())
-                {
-                    t_Area.customReticle = null;
-                    interactable = hit.collider.gameObject.GetComponent<XRGrabInteractable>();
-                    object_RB = interactable.GetComponent<Rigidbody>();
-                    original_Pos = interactable.transform.position;
-                    original_Rot = interactable.transform.rotation;
+                    return;
 
-                    if (interactable.transform.position != original_Pos)
-                    {
-                        //TODO: LERP INTERACTABLE BACK TO ORIGINAL POSITION
-                        //object_RB.isKinematic = false;
+                hit.collider.gameObject.AddComponent<XRGrabInteractable>();
+                interactable = hit.collider.gameObject.GetComponent<XRGrabInteractable>();
 
-                        //object_RB.transform.position = Vector3.Lerp(interactable.transform.position, original_Pos, smoothPosition * Time.deltaTime);
-                        //interactable.transform.rotation = Quaternion.Slerp(interactable.transform.rotation, original_Rot, smoothPosition * Time.deltaTime);
-                    }
-                    else
-                    {
-                        //object_RB.isKinematic = true;
-                    }
-                }
-            }                           
-        }
+                interactable.GetComponent<Rigidbody>().isKinematic = true;
+                interactable.attachTransform = this.transform;
+                interactable.attachEaseInTime = 2.0f;
+                interactable.smoothPosition = true;
+                interactable.smoothRotation = true;
 
-        if (Physics.Raycast(ray, out hit, line_Visual.lineLength) && !state_Manager.IsStateChange)
-        {            
-            DisplayLine(true, hit.point);
-        }
-        else
+                interactables.Add(interactable);
+            }
+        }        
+
+        if (Physics.Raycast(ray, out hit, line_Visual.lineLength))
         {
-            DisplayLine(false, transform.position);
+            if ( !state_Manager.IsStateChange)
+                DisplayLine(true, hit.point);
+            else
+                DisplayLine(false, transform.position);
+        }      
+    }
+
+    private void SetImmersiveInteractables()
+    {
+        if (state_Manager.currentState == PlayerStates.Immersive)
+        {
+            //if (t_Area)
+            //TODO: Dynamically enable teleport using raycast
+
+            if (interactables.Count > 0 && this.GetComponentInParent<XRSelectHandler>().has_Returned)
+            {
+                for (int i = 0; i < interactables.Count; i++)
+                {
+                    Destroy(interactables[i]);
+                    Destroy(interactables[i].GetComponent<Rigidbody>());
+                }
+
+                interactables.Clear();
+            }
         }
     }
 
     private void DisplayLine(bool display, Vector3 endpoint)
     {
         line_Visual.enabled = display;
-        line_Visual.lineLength = 6f;
+        line_Visual.lineLength = 8f;
 
         positions[0] = transform.position;
         positions[1] = endpoint;
--- NaviStruct/Assets/MyScripts/XRExtensions/XRSelectHandler.cs
+++ NaviStruct/Assets/MyScripts/XRExtensions/XRSelectHandler.cs
@@ -3,58 +3,100 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using Valve.VR.InteractionSystem;
 
 public class XRSelectHandler : MonoBehaviour
 {
-    public float smoothPosition = 5f;
-    public bool isReleased;
-    private XRRayInteractor ray_Interactor = null;
+    public float smoothPosition = 6f;
+    public float time = 2f;
 
-    private GameObject _interactable;
+    public bool is_Released = true;
+    public bool has_Returned = true;
+
+    private XRRayInteractor[] ray_Interactors = null;
+    private XRBaseInteractable _interactable;
     private Rigidbody object_RB;
     private Vector3 original_Pos;
     private Quaternion original_Rot;
 
     void Awake()
     {
-        ray_Interactor = GetComponent<XRRayInteractor>();
+        ray_Interactors = GetComponentsInChildren<XRRayInteractor>();      
+        //original_Pos = 
     }
 
     private void OnEnable()
     {
-        ray_Interactor.onSelectEnter.AddListener(OnSelectEnterHandler);
-        ray_Interactor.onSelectExit.AddListener(OnSelectExitHandler);
+        foreach(XRRayInteractor ray_Interactor in ray_Interactors)
+        {
+            ray_Interactor.onSelectEnter.AddListener(OnSelectEnterHandler);
+            ray_Interactor.onSelectExit.AddListener(OnSelectExitHandler);
+            ray_Interactor.onHoverEnter.AddListener(OnHoverEnterHandler);
+        }       
     }
 
     private void OnDisable()
     {
-        ray_Interactor.onSelectEnter.RemoveListener(OnSelectEnterHandler);
-        ray_Interactor.onSelectExit.RemoveListener(OnSelectExitHandler);        
+        foreach (XRRayInteractor ray_Interactor in ray_Interactors)
+        {
+            ray_Interactor.onSelectEnter.RemoveListener(OnSelectEnterHandler);
+            ray_Interactor.onSelectExit.RemoveListener(OnSelectExitHandler);
+            ray_Interactor.onHoverEnter.RemoveListener(OnHoverEnterHandler);
+        }            
+    }
+
+    public void OnHoverEnterHandler(XRBaseInteractable interactable)
+    {
+        if (!interactable.CompareTag("Floor") && has_Returned)
+        {
+            _interactable = interactable;
+            object_RB = _interactable.gameObject.GetComponent<Rigidbody>();
+            original_Pos = _interactable.gameObject.transform.position;
+            original_Rot = _interactable.gameObject.transform.rotation;
+        }
     }
 
     public void OnSelectEnterHandler(XRBaseInteractable interactable)
-    {
-        isReleased = false;
-        _interactable = interactable.gameObject;
-        object_RB = _interactable.GetComponent<Rigidbody>();
-        original_Pos = _interactable.transform.position;
-        original_Rot = _interactable.transform.rotation;
-
-        _interactable.GetComponent<XRGrabInteractable>().attachTransform = this.transform;        
+    {        
+        is_Released = false;
+        has_Returned = false;
     }
 
     public void OnSelectExitHandler(XRBaseInteractable interactable)
     {
-        isReleased = true;              
+        is_Released = true;              
     }
 
     private void FixedUpdate()
     {
-        if (isReleased && _interactable.transform.position != original_Pos)
+        if (is_Released && !has_Returned && object_RB != null)
         {
-            object_RB.MovePosition(Vector3.Lerp(_interactable.transform.position, original_Pos, Time.deltaTime * smoothPosition));
-            object_RB.MoveRotation(Quaternion.Slerp(_interactable.transform.rotation, original_Rot, Time.deltaTime * smoothPosition));
+            StartCoroutine(InteractableMove());
+        }            
+    }
+
+    private IEnumerator InteractableMove()
+    {
+        float i = 0.0f;
+        float rate = (1.0f / time) * smoothPosition;
+
+        while (i < 1)
+        {
+            i += Time.deltaTime * rate;
+
+            _interactable.enabled = false;
+            object_RB.MovePosition(Vector3.Lerp(_interactable.gameObject.transform.position, original_Pos, i));
+            object_RB.MoveRotation(Quaternion.Slerp(_interactable.gameObject.transform.rotation, original_Rot, i));
+
+            yield return null;
+        }                   
+
+        if(i == 1)
+        {
+            has_Returned = true;
+            _interactable.enabled = true;
         }
-            
+
+        yield return null;
     }
 }
